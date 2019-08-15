cd29b9b362c65058099d3bd1baaa61c79e4a84ac
Tim Molderez <id@timmolderez.be>
2019-08-16 01:56:54 +0200

    A selection menu is shown for ambiguous input

--- src/main.rs
+++ src/main.rs
@@ -2,45 +2,25 @@ use std::env;
 use std::io;
 extern crate dialoguer;
 extern crate dirs;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 mod alias;
+mod utils;
+use utils::*;
 use alias::Alias;
-use dialoguer::{theme::ColorfulTheme, theme::CustomPromptCharacterTheme, Input};
 use dialoguer::{Confirmation, Select};
 use std::fs;
 
 /// Jumpcut - a command-line utility to quickly access frequently-used commands/folders
+/// 
 /// Run without any parameters to display the usage message.
-fn main() {
-    // let selections = &[
-    //     "Ice Cream",
-    //     "Vanilla Cupcake",
-    //     "Chocolate Muffin",
-    //     "A Pile of sweet, sweet mustard",
-    // ];
-
-    // let selection = Select::new()
-    //     .with_prompt("Pick your flavor")
-    //     .default(0)
-    //     .items(&selections[..])
-    //     .interact_opt()
-    //     .unwrap();
-    // if let Some(selection) = selection {
-    //     println!("Enjoy your {}!", selections[selection]);
-    // } else {
-    //     println!("You didn't select anything!");
-    // }
-
+fn main() -> Result<(),io::Error> {
     let args: Vec<String> = env::args().collect();
     if args.len() == 1 {
         usage();
-        return;
+        return Ok(());
     }
 
-    match fs::create_dir_all(config_path()) {
-        Ok(_v) => (),
-        Err(_v) => error(&String::from("could not create configuration directory")),
-    };
+    fs::create_dir_all(config_path())?;
 
     let action = &args[1];
     match action.as_ref() {
@@ -51,16 +31,13 @@ fn main() {
         }
 
         "list" => {
-            match list_aliases() {
-                Ok(_) => (),
-                Err(e) => error(&e.to_string()),
-            };
+            return list_aliases();
         }
 
         "add" => {
             if args_ok(&args, 2) {
                 let cmd = args[3..].join(" ");
-                add_alias(&args[2], &cmd);
+                return add_alias(&args[2], &cmd);
             }
         }
 
@@ -68,30 +45,36 @@ fn main() {
             if args_ok(&args, 2) {
                 let abs_pwd = absolute_path(env::current_dir().unwrap());
                 let cmd = args[3..].join(" ");
-                add_alias(&args[2], &format!("cd {};{}", abs_pwd, cmd));
+                return add_alias(&args[2], &format!("cd {};{}", abs_pwd, cmd));
             }
         }
 
         "addpath" => {
             if args_ok(&args, 2) {
                 let abs_path = absolute_path(PathBuf::from(args[3].clone()));
-                add_alias(&args[2],
-                        &format!("cd {}", abs_path),
-                    );
+                return add_alias(&args[2], &format!("cd {}", abs_path));
             }
         }
 
         "desc" => {
             if args_ok(&args, 2) {
                 let desc = args[3..].join(" ");
-                add_description(&args[2], &desc);
+                return add_description(&args[2], &desc);
             }
         }
 
-        "rm" => remove_alias(&args[2]),
+        "rm" => {
+            if args_ok(&args, 1) {
+                return remove_alias(&args[2]);
+            }
+        },
 
-        _ => exec_alias(action, args[2..].to_vec()),
+        _ => {
+            return find_and_exec_alias(action, args[2..].to_vec());
+        }
     };
+
+    return Ok(());
 }
 
 fn is_reserved_keyword(a: &String) -> bool {
@@ -108,6 +91,7 @@ fn list_aliases() -> io::Result<()> {
     let path = config_path();
     let entries = path.read_dir()?;
 
+    // Find the length of the longest alias; we need this for formatting the output
     let alias_len = entries.fold(0, |current_max, entry| {
         return match entry {
             Ok(file) => {
@@ -138,6 +122,43 @@ fn list_aliases() -> io::Result<()> {
     return Ok(());
 }
 
+fn find_and_exec_alias(alias: &String, args: Vec<String>) -> io::Result<()> {
+    let path = config_path().join(alias);
+    if path.exists() {
+        exec_alias(alias, args);
+    } else {
+        let matches = config_path().read_dir()?.filter_map(|f| {
+            let entry = f.unwrap();
+            let fname_str = entry.file_name().as_os_str().to_os_string().into_string().unwrap();
+            if fname_str.contains(alias) {
+                return Some(fname_str);
+            } else {
+                return None;
+            }
+        });
+        
+        let match_vec: Vec<String> = matches.collect();
+        match match_vec.len() {
+            0 => (),
+            1 => {
+                exec_alias(&match_vec[0], args);
+            },
+            _ => {
+                let selection = Select::new()
+                    .default(0)
+                    .items(&match_vec[..])
+                    .interact_opt()
+                    .unwrap();
+                if let Some(selection) = selection {
+                    exec_alias(&match_vec[selection], args);
+                }
+            }
+        }
+        
+    }
+    return Ok(());
+}
+
 fn exec_alias(alias: &String, args: Vec<String>) {
     let path = config_path().join(alias);
     match Alias::read(&alias, &path) {
@@ -146,13 +167,10 @@ fn exec_alias(alias: &String, args: Vec<String>) {
     }
 }
 
-fn add_alias(alias: &String, cmd: &String) {
+fn add_alias(alias: &String, cmd: &String) -> io::Result<()> {
     if is_reserved_keyword(alias) {
-        error(&format!(
-            "\"{}\" cannot be used as an alias name; it is a reserved keyword.",
-            alias
-        ));
-        return;
+        error(&format!("\"{}\" cannot be used as an alias name; it is a reserved keyword.", alias));
+        return Err(io::Error::new(io::ErrorKind::Other, "Reserved keyword."));
     }
 
     let al = Alias::new(alias.clone(), cmd.clone(), "".to_string());
@@ -163,100 +181,28 @@ fn add_alias(alias: &String, cmd: &String) {
             .interact()
         {
             Ok(_v) => {
-                match al.write(&path) {
-                    Ok(_) => (),
-                    Err(_) => error(&format!("Could not save alias \"{}\".", alias)),
-                };
+                return al.write(&path);
             }
-            Err(_v) => (),
+            Err(_v) => Ok(()),
         }
     } else {
-        match al.write(&path) {
-            Ok(_) => (),
-            Err(_) => error(&format!("Could not save alias \"{}\".", alias)),
-        };
+        return al.write(&path);
     }
 }
 
-fn add_description(alias: &String, description: &String) {
+fn add_description(alias: &String, description: &String) -> io::Result<()> {
     let path = config_path().join(alias);
-    println!("{}", path.display());
-    match Alias::read(&alias, &path) {
-        Ok(al) => {
-            let new_al = Alias::new(alias.clone(), al.get_command().clone(), description.clone());
-            match new_al.write(&path) {
-                Ok(_) => (),
-                Err(e) => error(&e.to_string()),
-            }
-        }
-        Err(e) => error(&e.to_string()),
-    }
+    let al = Alias::read(&alias, &path)?;
+    let new_al = Alias::new(alias.clone(), al.get_command().clone(), description.clone());
+    return new_al.write(&path);
 }
 
-fn remove_alias(alias: &String) {
+fn remove_alias(alias: &String) -> io::Result<()> {
     let path = config_path().join(alias);
     if path.exists() {
-        match fs::remove_file(path) {
-            Ok(_v) => (),
-            Err(_v) => error(&format!("Could not remove alias \"{}\".", alias)),
-        }
+        return fs::remove_file(path);
     } else {
         error(&format!("There is no alias named \"{}\".", alias));
+        return Err(io::Error::new(io::ErrorKind::NotFound, "Alias not found."));
     }
-}
-
-/// Returns path to config directory
-fn config_path() -> PathBuf {
-    let home = dirs::home_dir().unwrap_or_default();
-    return home.join(".jumpcut");
-}
-
-fn absolute_path(path: PathBuf) -> String {
-    match fs::canonicalize(path) {
-        Ok(v) => {
-            let abs_path = v.into_os_string().into_string().unwrap();
-            if abs_path.starts_with("\\\\?\\") {
-                /* On Windows, Rust usually works with the "extended length path" / UNC path format, which has a \\?\  prefix.
-                While perfectly fine, if you `cd` to such a path in Powershell, this absurdly long "Microsoft.PowerShell.Core\FileSystem::\\?\" prefix
-                is shown in your shell. Because of this, I'm stripping the \\?\ prefix to convert it back to a normal path.
-                The only caveat to normal paths is that they usually have a 260 max. character limit: 
-                https://docs.microsoft.com/en-us/windows/win32/fileio/naming-a-file#maximum-path-length-limitation */
-                return abs_path[4..].to_string();
-            }
-            return abs_path;
-        },
-        Err(_v) => {
-            error(&String::from("the provided path does not exist"));
-            panic!();
-        }
-    }
-}
-
-fn args_ok(args: &Vec<String>, num: usize) -> bool {
-    if args.len() >= num + 2 {
-        return true;
-    } else {
-        error(&format!("incorrect number of arguments; {} expected", num));
-        return false;
-    }
-}
-
-fn error(err: &String) {
-    println!("Error: {}", err);
-    usage();
-}
-
-/// Prints usage message
-fn usage() {
-    print!("
-Jumpcut usage:
-
-    j list                      List all aliases
-    j [alias]                   Execute the alias named [alias] (also works by entering only part of its name)
-    j [alias] [arg-1]..[arg-n]  Execute [alias], using the given arguments
-    j add [alias] [cmd]         Adds a new alias, which executes the given command (arguments can be specified using $1, $2, ..)
-    j addwd [alias] [cmd]       Adds a new alias, which always executes the given command from the current working directory
-    j addpath [alias] [path]    Adds a new alias, which navigates to the given path
-    j desc [alias] [desc]       Sets the description of [alias]
-    j rm [alias]                Removes [alias]")
 }
